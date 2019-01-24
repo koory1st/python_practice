b1a0dc37d78056f8c8c8aeb1845c8ffea3f3f53b
koory1st <32436334@qq.com>
2019-01-24 23:12:45 +0800

    程序启动参数

---
+++ p002.py
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+
+# 程序启动参数
+from sys import argv
+script, first, second, third = argv
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
